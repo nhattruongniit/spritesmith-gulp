760d7e3c7b6fb12d1b5f98c66f5643c1f9d8e628
unknown <nhattruongniit@yahoo.com.vn>
2018-12-19 23:11:44 +0700

    teset

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = new require('gulp');
 var spritesmith = new require('gulp.spritesmith');
-var spritesmithTexturepacker = new require('spritesmith-texturepacker')
+var spritesmithTexturepacker = new require('spritesmith-texturepacker');
+var gulpif = new require("gulp-if");
 
 var cardPath = './images/flip-card';
 var stages = [
@@ -19,4 +20,18 @@ gulp.task('sprite', function() {
             }))
             .pipe(gulp.dest('./dist/' + cardPath + '/' + stage))
     })
+});
+
+gulp.task('spritesmith', function () {
+   return  gulp.src('./images/flip-card/avatar/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprites.png',
+            padding: 2,
+            cssName: 'spritesheet.scss',
+            imgPath: '/dist/images/avatar/sprites.png',
+            algorithm: 'binary-tree',
+            cssTemplate: 'node_modules/gulp.spritesmith/docs/handlebarsInheritance.scss.handlebars'
+        }))
+        .pipe(gulpif('*.png', gulp.dest('./dist/images/avatar/')))
+        .pipe(gulpif('*.scss', gulp.dest('./dist/sass/')))
 });
